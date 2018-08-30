1f5296f7d51fa81588e08ff0c2e3cb1c8e47ea4f
Øyvind Kolås <pippin@gimp.org>
2018-08-30 02:01:18 +0200

    babl-fish: handle case of no from_list in source

--- babl/babl-fish.c
+++ babl/babl-fish.c
@@ -120,7 +120,8 @@ babl_conversion_find (const void *source,
 {
   void *data = (void*)destination;
 
-  babl_list_each (BABL (source)->type.from_list, match_conversion, &data);
+  if (BABL (source)->type.from_list)
+    babl_list_each (BABL (source)->type.from_list, match_conversion, &data);
   if (data != (void*)destination) /* didn't change */
     return data;
   data = NULL;
