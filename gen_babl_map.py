a04535b0964d2bd40d08e4b3d16ff27b6ee7262c
Alex Samorukov <samm@os2.kiev.ua>
2019-10-31 08:14:49 +0100

    Fix darwin/clang map

--- gen_babl_map.py
+++ gen_babl_map.py
@@ -2,6 +2,7 @@ import sys
 
 export_symbols=sys.argv[1]
 version_file=sys.argv[2]
+version_file_clang=sys.argv[2] + ".clang"
 
 with open(export_symbols, 'r') as syms, \
      open(version_file, 'w') as version:
@@ -9,3 +10,8 @@ with open(export_symbols, 'r') as syms, \
      for sym in syms:
         version.write("        {};\n".format(sym.strip()))
      version.write("    local:\n        *;\n};\n")
+
+with open(export_symbols, 'r') as syms, \
+     open(version_file_clang, 'w') as version:
+     for sym in syms:
+        version.write("_{}\n".format(sym.strip()))
