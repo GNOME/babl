8c6f68cb2fbbea3b22e1a2ad7aafad43251dc59a
Bruno <brunvonlope@outlook.com>
2024-12-10 16:01:24 -0300

    meson: Use proper 'vs_module_defs' to export table
    We already do that on GIMP project. Without an
    .def file, MS 'link' fails to link the binaries.

--- gen_babl_map.py
+++ gen_babl_map.py
@@ -9,11 +9,15 @@ version_file_clang=sys.argv[2] + ".clang"
 with open(export_symbols, 'r') as syms, \
      open(version_file, 'w') as version:
      version.write("V0_1_0 {\n    global:\n")
+     next(syms)
      for sym in syms:
-        version.write("        {};\n".format(sym.strip()))
+       final_sym = sym.replace('  ', '')
+       version.write("        {};\n".format(final_sym.strip()))
      version.write("    local:\n        *;\n};\n")
 
 with open(export_symbols, 'r') as syms, \
      open(version_file_clang, 'w') as version:
+     next(syms)
      for sym in syms:
-        version.write("_{}\n".format(sym.strip()))
+       final_sym = sym.replace('  ', '')
+       version.write("_{}\n".format(final_sym.strip()))
